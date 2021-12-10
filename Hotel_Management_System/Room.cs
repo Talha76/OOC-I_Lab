e34d77b133066645212fa54f9a145dfab0878b7c
Tasrefee Mahmood Ridun <95172466+Ridun-50@users.noreply.github.com>
2021-12-10 21:25:03 +0600

    Added lab 4 task

---
+++ Hotel_Management_System/Room.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp1
+{
+    internal class Room
+    {
+        protected string name;
+        protected int tv, fridge, bed, quantity;
+        protected bool ac;
+        public string get_name() { return this.name; }
+        public int get_count() { return this.quantity; }
+        public void set_count(int quantity) { this.quantity = quantity; }
+        protected Room() { }
+    }
+}
---
+++ Hotel_Management_System/SingleRoom.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp1
+{
+    internal class SingleRoom: Room
+    {
+        private bool gaming;
+        public int calculate_cost()
+        {
+            return 2000 + (this.ac ? 500 : 0) + (this.gaming ? 1000 : 0);
+        }
+
+        public SingleRoom(string name, string tv, string fridge, string qty, string ac, string gaming)
+        {
+            this.name = name;
+            this.tv = (tv == "" ? 1 : Convert.ToInt32(tv));
+            this.fridge = (fridge == "" ? 1 : Convert.ToInt32(fridge));
+            this.bed = 1;
+            this.ac = (ac == "Yes" ? true : false);
+            this.quantity = Convert.ToInt32(qty);
+            this.gaming = (gaming == "Yes" ? true : false);
+        }
+    }
+}
