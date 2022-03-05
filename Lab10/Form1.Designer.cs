6e58714805219fb9b1d7913a9eea580eb4bb6681
Mushfiq Talha <58101262+Talha76@users.noreply.github.com>
2022-03-05 07:08:30 +0600

    Uploaded Lab 10

---
+++ Lab10/Form1.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace Lab10
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.findEmailTextbox = new System.Windows.Forms.TextBox();
+            this.showButton = new System.Windows.Forms.Button();
+            this.userInfoListbox = new System.Windows.Forms.ListBox();
+            this.allUserListbox = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // findEmailTextbox
+            // 
+            this.findEmailTextbox.Location = new System.Drawing.Point(133, 24);
+            this.findEmailTextbox.Name = "findEmailTextbox";
+            this.findEmailTextbox.Size = new System.Drawing.Size(295, 26);
+            this.findEmailTextbox.TabIndex = 0;
+            // 
+            // showButton
+            // 
+            this.showButton.Location = new System.Drawing.Point(242, 65);
+            this.showButton.Name = "showButton";
+            this.showButton.Size = new System.Drawing.Size(75, 32);
+            this.showButton.TabIndex = 1;
+            this.showButton.Text = "Show";
+            this.showButton.UseVisualStyleBackColor = true;
+            this.showButton.Click += new System.EventHandler(this.showInfo);
+            // 
+            // userInfoListbox
+            // 
+            this.userInfoListbox.FormattingEnabled = true;
+            this.userInfoListbox.ItemHeight = 20;
+            this.userInfoListbox.Location = new System.Drawing.Point(18, 116);
+            this.userInfoListbox.Name = "userInfoListbox";
+            this.userInfoListbox.Size = new System.Drawing.Size(517, 264);
+            this.userInfoListbox.TabIndex = 2;
+            // 
+            // allUserListbox
+            // 
+            this.allUserListbox.FormattingEnabled = true;
+            this.allUserListbox.ItemHeight = 20;
+            this.allUserListbox.Location = new System.Drawing.Point(541, 12);
+            this.allUserListbox.Name = "allUserListbox";
+            this.allUserListbox.Size = new System.Drawing.Size(685, 584);
+            this.allUserListbox.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1370, 614);
+            this.Controls.Add(this.allUserListbox);
+            this.Controls.Add(this.userInfoListbox);
+            this.Controls.Add(this.showButton);
+            this.Controls.Add(this.findEmailTextbox);
+            this.Name = "Form1";
+            this.Text = "User Finder";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox findEmailTextbox;
+        private System.Windows.Forms.Button showButton;
+        private System.Windows.Forms.ListBox userInfoListbox;
+        private System.Windows.Forms.ListBox allUserListbox;
+    }
+}
+
---
+++ Lab10/Form1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Lab10
+{
+    public partial class Form1 : Form
+    {
+        List<User> userList = new List<User>();
+        public Form1()
+        {
+            InitializeComponent();
+            using (var reader = new StreamReader(@"E:\SWE-4202-OOC-I_Lab\Lab10\userInfo.csv"))
+            {
+                while(!reader.EndOfStream)
+                {
+                    var line = reader.ReadLine();
+                    var values = line.Split(',');
+                    string toAddinList = "";
+                    for(int i = 0; i < 2; i++)
+                    {
+                        toAddinList += values[i] + " ";
+                    }
+                    allUserListbox.Items.Add(toAddinList);
+
+                    User dummy = new User(values[0], values[1], values[2], values[3], values[4],
+                        values[5], values[6], values[7], values[8], values[9]);
+                    userList.Add(dummy);
+                }
+            }
+        }
+
+        private void showInfo(object sender, EventArgs e)
+        {
+            userInfoListbox.ResetText();
+
+            User dummy = null;
+            foreach(User user in userList)
+            {
+                if(user.eMail == findEmailTextbox.Text)
+                    dummy = user;
+            }
+
+            string toShow = findEmailTextbox.Text + '_' + DateTime.Now.ToString() + '_';
+            if (dummy == null)
+            {
+                toShow += "NotFound";
+                MessageBox.Show("User Not found!");
+            }
+            else
+            {
+                userInfoListbox.Items.Add("Name: " + dummy.firstName + " " + dummy.lastName);
+                userInfoListbox.Items.Add("Address: " + dummy.Address);
+                userInfoListbox.Items.Add("City: " + dummy.City);
+                userInfoListbox.Items.Add("Country: " + dummy.Country);
+                userInfoListbox.Items.Add("State: " + dummy.State);
+                userInfoListbox.Items.Add("Zip: " + dummy.Zip);
+                userInfoListbox.Items.Add("Phone 1: " + dummy.Phone1);
+                userInfoListbox.Items.Add("Phone 2: " + dummy.Phone2);
+                userInfoListbox.Items.Add("Email: " + dummy.eMail);
+                //userInfoListbox.Items.Add(dummy.getDetailedInfo());
+                toShow += dummy.firstName + "_" + dummy.lastName + "_" + dummy.Address + "_"
+                    + dummy.City + "_" + dummy.Country + "_" + dummy.State + "_" + dummy.Zip + "_"
+                    + dummy.Phone1 + "_" + dummy.Phone2 + "_" + dummy.eMail;
+            }
+            string path = @"E:\SWE-4202-OOC-I_Lab\Lab10\search.txt";
+            if (!File.Exists(path))
+            {
+                using (StreamWriter sw = File.CreateText(path))
+                {
+                    sw.WriteLine(toShow);
+                }
+            }
+            else
+            {
+                using (StreamWriter sr = File.AppendText(path))
+                {
+                    sr.WriteLine(toShow);
+                }
+            }
+        }
+    }
+}
