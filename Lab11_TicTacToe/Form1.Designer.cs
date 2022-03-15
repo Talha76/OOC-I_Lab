5c3f481712e212f6dd04b23b5d52c20d3b75e07d
Mushfiq Talha <58101262+Talha76@users.noreply.github.com>
2022-03-15 09:05:49 +0600

    Created the easy version.

---
+++ Lab11_TicTacToe/Form1.Designer.cs
@@ -0,0 +1,346 @@
+﻿namespace Lab11_TicTacToe
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.second = new System.Windows.Forms.RadioButton();
+            this.first = new System.Windows.Forms.RadioButton();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.x = new System.Windows.Forms.RadioButton();
+            this.o = new System.Windows.Forms.RadioButton();
+            this.label1 = new System.Windows.Forms.Label();
+            this.scorePlayer = new System.Windows.Forms.Label();
+            this.scoreCom = new System.Windows.Forms.Label();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.difficult = new System.Windows.Forms.RadioButton();
+            this.izy = new System.Windows.Forms.RadioButton();
+            this.button10 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.button1.Location = new System.Drawing.Point(36, 314);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 71);
+            this.button1.TabIndex = 0;
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.buttonClick);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.button2.Location = new System.Drawing.Point(117, 314);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 71);
+            this.button2.TabIndex = 1;
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.buttonClick);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.button3.Location = new System.Drawing.Point(198, 314);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 71);
+            this.button3.TabIndex = 2;
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.buttonClick);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.button4.Location = new System.Drawing.Point(36, 391);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 71);
+            this.button4.TabIndex = 5;
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.buttonClick);
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.button5.Location = new System.Drawing.Point(117, 391);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 71);
+            this.button5.TabIndex = 4;
+            this.button5.UseVisualStyleBackColor = false;
+            this.button5.Click += new System.EventHandler(this.buttonClick);
+            // 
+            // button6
+            // 
+            this.button6.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.button6.Location = new System.Drawing.Point(198, 391);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(75, 71);
+            this.button6.TabIndex = 3;
+            this.button6.UseVisualStyleBackColor = false;
+            this.button6.Click += new System.EventHandler(this.buttonClick);
+            // 
+            // button7
+            // 
+            this.button7.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.button7.Location = new System.Drawing.Point(36, 468);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(75, 71);
+            this.button7.TabIndex = 8;
+            this.button7.UseVisualStyleBackColor = false;
+            this.button7.Click += new System.EventHandler(this.buttonClick);
+            // 
+            // button8
+            // 
+            this.button8.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.button8.Location = new System.Drawing.Point(117, 468);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(75, 71);
+            this.button8.TabIndex = 7;
+            this.button8.UseVisualStyleBackColor = false;
+            this.button8.Click += new System.EventHandler(this.buttonClick);
+            // 
+            // button9
+            // 
+            this.button9.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.button9.Location = new System.Drawing.Point(198, 468);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(75, 71);
+            this.button9.TabIndex = 6;
+            this.button9.UseVisualStyleBackColor = false;
+            this.button9.Click += new System.EventHandler(this.buttonClick);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.second);
+            this.groupBox1.Controls.Add(this.first);
+            this.groupBox1.Location = new System.Drawing.Point(36, 24);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(237, 73);
+            this.groupBox1.TabIndex = 9;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Select your turn";
+            // 
+            // second
+            // 
+            this.second.AutoSize = true;
+            this.second.Location = new System.Drawing.Point(110, 26);
+            this.second.Name = "second";
+            this.second.Size = new System.Drawing.Size(89, 24);
+            this.second.TabIndex = 1;
+            this.second.TabStop = true;
+            this.second.Text = "Second";
+            this.second.UseVisualStyleBackColor = true;
+            // 
+            // first
+            // 
+            this.first.AutoSize = true;
+            this.first.Location = new System.Drawing.Point(17, 26);
+            this.first.Name = "first";
+            this.first.Size = new System.Drawing.Size(65, 24);
+            this.first.TabIndex = 0;
+            this.first.TabStop = true;
+            this.first.Text = "First";
+            this.first.UseVisualStyleBackColor = true;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.x);
+            this.groupBox2.Controls.Add(this.o);
+            this.groupBox2.Location = new System.Drawing.Point(36, 104);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(237, 71);
+            this.groupBox2.TabIndex = 10;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Select your marker";
+            // 
+            // x
+            // 
+            this.x.AutoSize = true;
+            this.x.Location = new System.Drawing.Point(110, 25);
+            this.x.Name = "x";
+            this.x.Size = new System.Drawing.Size(45, 24);
+            this.x.TabIndex = 3;
+            this.x.TabStop = true;
+            this.x.Text = "X";
+            this.x.UseVisualStyleBackColor = true;
+            // 
+            // o
+            // 
+            this.o.AutoSize = true;
+            this.o.Location = new System.Drawing.Point(17, 25);
+            this.o.Name = "o";
+            this.o.Size = new System.Drawing.Size(46, 24);
+            this.o.TabIndex = 2;
+            this.o.TabStop = true;
+            this.o.Text = "O";
+            this.o.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Mongolian Baiti", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(400, 94);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(72, 25);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "Score";
+            // 
+            // scorePlayer
+            // 
+            this.scorePlayer.AutoSize = true;
+            this.scorePlayer.Location = new System.Drawing.Point(401, 131);
+            this.scorePlayer.Name = "scorePlayer";
+            this.scorePlayer.Size = new System.Drawing.Size(56, 20);
+            this.scorePlayer.TabIndex = 12;
+            this.scorePlayer.Text = "Player:";
+            // 
+            // scoreCom
+            // 
+            this.scoreCom.AutoSize = true;
+            this.scoreCom.Location = new System.Drawing.Point(393, 166);
+            this.scoreCom.Name = "scoreCom";
+            this.scoreCom.Size = new System.Drawing.Size(83, 20);
+            this.scoreCom.TabIndex = 13;
+            this.scoreCom.Text = "Computer:";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.difficult);
+            this.groupBox3.Controls.Add(this.izy);
+            this.groupBox3.Location = new System.Drawing.Point(36, 191);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(237, 71);
+            this.groupBox3.TabIndex = 11;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Select your level";
+            // 
+            // difficult
+            // 
+            this.difficult.AutoSize = true;
+            this.difficult.Location = new System.Drawing.Point(110, 25);
+            this.difficult.Name = "difficult";
+            this.difficult.Size = new System.Drawing.Size(87, 24);
+            this.difficult.TabIndex = 3;
+            this.difficult.TabStop = true;
+            this.difficult.Text = "Difficult";
+            this.difficult.UseVisualStyleBackColor = true;
+            // 
+            // izy
+            // 
+            this.izy.AutoSize = true;
+            this.izy.Location = new System.Drawing.Point(17, 25);
+            this.izy.Name = "izy";
+            this.izy.Size = new System.Drawing.Size(69, 24);
+            this.izy.TabIndex = 2;
+            this.izy.TabStop = true;
+            this.izy.Text = "Easy";
+            this.izy.UseVisualStyleBackColor = true;
+            // 
+            // button10
+            // 
+            this.button10.Location = new System.Drawing.Point(117, 269);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(75, 39);
+            this.button10.TabIndex = 14;
+            this.button10.Text = "Start";
+            this.button10.UseVisualStyleBackColor = true;
+            this.button10.Click += new System.EventHandler(this.startGame);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(574, 572);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.scoreCom);
+            this.Controls.Add(this.scorePlayer);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Tic Tac Toe";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton second;
+        private System.Windows.Forms.RadioButton first;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.RadioButton x;
+        private System.Windows.Forms.RadioButton o;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label scorePlayer;
+        private System.Windows.Forms.Label scoreCom;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.RadioButton difficult;
+        private System.Windows.Forms.RadioButton izy;
+        private System.Windows.Forms.Button button10;
+    }
+}
+
---
+++ Lab11_TicTacToe/Form1.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace Lab11_TicTacToe
+{
+    public partial class Form1 : Form
+    {
+        bool playerTurn;
+        string player, com;
+        bool easy;
+        List<Button> buttons;
+        List<int> unused;
+        Random rand;
+        int pWin = 0, cWin = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        void createUnusedList()
+        {
+            unused.Clear();
+            for(int i = 0; i < 9; i++)
+            {
+                if (buttons[i].Text == "")
+                    unused.Add(i);
+            }
+        }
+
+        void easyCom()
+        {
+            bool allUsed = true;
+            foreach (Button i in buttons)
+                if (i.Text == "")
+                    allUsed = false;
+            if (allUsed)
+            {
+                return;
+            }
+            buttons[unused[rand.Next(unused.Count)]].Text = com;
+        }
+
+        int checkIfPlayerWins()
+        {
+            for (int i = 0; i < 9; i += 3)
+            {
+                if (buttons[i].Text == buttons[i + 1].Text && buttons[i + 2].Text == buttons[i].Text
+                    && buttons[i].Text != "")
+                {
+                    return buttons[i].Text == player ? 1 : 0;
+                }
+            }
+            for (int i = 0; i < 3; i++)
+            {
+                if (buttons[i].Text == buttons[i + 3].Text && buttons[i + 6].Text == buttons[i].Text
+                    && buttons[i].Text != "")
+                {
+                    return buttons[i].Text == player ? 1 : 0;
+                }
+            }
+            if (buttons[0].Text == buttons[4].Text && buttons[8].Text == buttons[0].Text
+                    && buttons[0].Text != "")
+            {
+                return buttons[0].Text == player ? 1 : 0;
+            }
+            if (buttons[2].Text == buttons[4].Text && buttons[6].Text == buttons[2].Text
+                    && buttons[2].Text != "")
+            {
+                return buttons[2].Text == player ? 1 : 0;
+            }
+            return -1;
+        }
+
+        void clean()
+        {
+            foreach(Button i in buttons)
+                i.Text = "";
+        }
+
+        private async void buttonClick(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+            if (!string.IsNullOrEmpty(button.Text))
+            {
+                MessageBox.Show("You cannot make a move on a used square.");
+                return;
+            }
+
+            button.Text = player;
+            await Task.Delay(1000);
+
+            createUnusedList();
+            if (easy)
+            {
+                easyCom();
+            }
+
+            int check = checkIfPlayerWins();
+            if (check == 1)
+            {
+                MessageBox.Show("Player Wins");
+                pWin++;
+                scorePlayer.Text = "Player: " + pWin.ToString();
+            }
+            else if (check == 0)
+            {
+                MessageBox.Show("Computer Wins.");
+                cWin++;
+                scoreCom.Text = "Computer: " + cWin.ToString();
+            }
+
+            bool allUsed = true;
+            foreach (Button i in buttons)
+                if (i.Text == "")
+                    allUsed = false;
+            if (allUsed && check == -1)
+            {
+                MessageBox.Show("Match Tied.");
+            }
+        }
+
+        private void startGame(object sender, EventArgs e)
+        {
+            if (o.Checked)
+            {
+                player = "O"; com = "X";
+            }
+            else
+            {
+                player = "X"; com = "O";
+            }
+
+            playerTurn = first.Checked;
+            easy = izy.Checked;
+
+            buttons = new List<Button>() { button1, button2, button3, button4, button5, button6, button7, button8,
+                button9 };
+            unused = new List<int>();
+            rand = new Random();
+            clean();
+
+            if(!playerTurn)
+            {
+                int i = rand.Next(0, 9);
+                buttons[i].Text = com;
+            }
+
+            MessageBox.Show("You may start the game.");
+        }
+    }
+}
